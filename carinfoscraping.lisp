fa93f6700c8ec4b8b3f89c35258909f824ba7ebc
Morten Høgh <mortenhogh@gmai.com>
2016-12-20 22:56:10 +0100

    yason parsing.

--- carinfoscraping.lisp
+++ carinfoscraping.lisp
@@ -4,16 +4,19 @@
 
 ;;; "carinfoscraping" goes here. Hacks and glory await!
 
-(defun test ()
-    (tinglysning-request "https://www.tinglysning.dk/tinglysning/unsecrest/soegbil?stelnr=WVWZZZ1HZSB012475"))
+(defun init ()
+    (parse-request (tinglysning-request "https://www.tinglysning.dk/tinglysning/unsecrest/soegbil?stelnr=WVWZZZ1HZSB012475")))
 
 (defun tinglysning-request (url)
-  (let ((stream (drakma:http-request url
+  (drakma:http-request url
                        :accept "application/json"
                        :method :get
                        :external-format-out :utf-8
                        :external-format-in :utf-8
                        :redirect 100
-                       :want-stream t)))
-    (yason:parse stream)))
+                       :want-stream t))
  
+(defun parse-request (stream)
+  (let ((yason:*parse-object-as* :alist))
+    (assoc 'uuid (yason:parse stream))))
+
