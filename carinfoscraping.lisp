ce75e2073af5e48f9e6515ecbcd0394f3c64c3a6
Morten Høgh <mortenhogh@gmail.com>
2016-12-27 21:08:38 +0100

    Error handling added.

--- carinfoscraping.lisp
+++ carinfoscraping.lisp
@@ -5,7 +5,12 @@
 ;;; "carinfoscraping" goes here. More hacks and glory await!
 
 (defun i ()
-  (parse-request-as (http-get "https://www.tinglysning.dk/tinglysning/unsecrest/soegbil?stelnr=WVWZZZ1HZSB012475")))
+  (handler-case
+      (parse-request-as (http-get "https://www.tinglysning.dk/tinglysning/unsecrest/soegbil?stelnr=WVWZZZ1HZSB012475"))
+    (on-response-not-ok (ex)
+      (format t "An error happened: ~a~%" (text ex)))))
+
+    
 
 (defun http-get (url)
   (let ((drakma:*text-content-types* (cons '("application" . "json")
@@ -19,13 +24,19 @@
                          :external-format-out :utf-8
                          :external-format-in :utf-8
                          :redirect 100)
-      (cond ((not (equalp status-code 200)) (error request-result))
-            ((not (string= (cdr (assoc ':content-type headers)) "application/json; charset=UTF-8")) (error request-result)) 
+      (cond ((not (equalp status-code 201))
+             (error 'on-response-not-ok :text (concatenate 'string "HTTP error: " (write-to-string status-code))))
+            ((not (string= (cdr (assoc ':content-type headers)) "application/json; charset=UTF-8"))
+             (error 'on-response-not-ok :text "Not JSON content.")) 
             (t request-result)))))
  
 (defun parse-request-as (string)
-    (json:encode-json string)))
+    (json:encode-json string))
 
+(define-condition on-response-not-ok (error)
+  ((text :initarg :text :reader text))) 
 
+(define-condition on-response-not-json (error)
+  ((text :initarg :text :reader text))) 
 
 
