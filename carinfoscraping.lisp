5d411dd637729348ff5095c4931a5e2dbbda4147
Morten Høgh <mortenhogh@gmail.com>
2016-12-28 17:46:21 +0100

    Refactoring.

--- carinfoscraping.lisp
+++ carinfoscraping.lisp
@@ -6,13 +6,13 @@
 
 (defun i ()
   (handler-case
-      (parse-request-as (http-get "https://www.tinglysning.dk/tinglysning/unsecrest/soegbil?stelnr=WVWZZZ1HZSB012475"))
+      (parse-request (http-get-json "https://www.tinglysning.dk/tinglysning/unsecrest/soegbil?stelnr=WVWZZZ1HZSB012475"))
     (on-response-not-ok (ex)
       (format t "An error happened: ~a~%" (text ex)))))
 
     
 
-(defun http-get (url)
+(defun http-get-json (url)
   (let ((drakma:*text-content-types* (cons '("application" . "json")
                                            drakma:*text-content-types*))
 	(drakma:*drakma-default-external-format* :utf-8))
@@ -24,14 +24,21 @@
                          :external-format-out :utf-8
                          :external-format-in :utf-8
                          :redirect 100)
-      (cond ((not (equalp status-code 201))
+      (cond ((not (equalp status-code 200))
              (error 'on-response-not-ok :text (concatenate 'string "HTTP error: " (write-to-string status-code))))
             ((not (string= (cdr (assoc ':content-type headers)) "application/json; charset=UTF-8"))
              (error 'on-response-not-ok :text "Not JSON content.")) 
             (t request-result)))))
  
-(defun parse-request-as (string)
-    (json:encode-json string))
+(defun parse-request (json-string)
+  (let ((data (json:decode-json-from-string json-string)))
+    (get-uuid data)))
+
+(defun get-uuid (alist)
+  (cdr (assoc :UUID (first (get-items alist)))))
+
+(defun get-items (alist)
+  (rest (assoc :ITEMS alist)))
 
 (define-condition on-response-not-ok (error)
   ((text :initarg :text :reader text))) 
