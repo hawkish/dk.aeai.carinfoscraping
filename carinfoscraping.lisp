64c56e69578c097537f7efa3cda395e2fb16bbb4
Morten Høgh <mortenhogh@gmail.com>
2016-12-29 13:52:42 +0100

    Refactoring.

--- carinfoscraping.lisp
+++ carinfoscraping.lisp
@@ -6,16 +6,40 @@
 
 (defun i ()
   (handler-case
-      (parse-request (http-get-json "https://www.tinglysning.dk/tinglysning/unsecrest/soegbil?stelnr=WVWZZZ1HZSB012475"))
-    (on-response-not-ok (ex)
-      (format t "An error happened: ~a~%" (text ex)))))
+      (progn 
+        (let ((response (first-request "https://www.tinglysning.dk/tinglysning/unsecrest/soegbil?stelnr=WVWZZZ1HZSB012475")))
+          (let ((uuid (get-uuid (parse-request (first response))))
+                (cookie-jar (last response)))
+            (print uuid)
+            (print cookie-jar)
+            (let ((second-response (second-request (concatenate 'string "https://www.tinglysning.dk/tinglysning/insecrest/bil/uuid/" uuid "?xhtml=true") cookie-jar)))
+              (print second-response)))))))
+              ;;(on-response-not-ok (ex)
+      ;;(format t "An error happened: ~a~%" (text ex)))))
 
-    
-
-(defun http-get-json (url)
+(defun second-request (url cookie-jar)
   (let ((drakma:*text-content-types* (cons '("application" . "json")
                                            drakma:*text-content-types*))
 	(drakma:*drakma-default-external-format* :utf-8))
+    (multiple-value-bind (request-result status-code headers)
+        (drakma:http-request url
+                         :accept "application/json"
+                         :method :get
+                         :external-format-out :utf-8
+                         :external-format-in :utf-8
+                         :redirect 100
+                         :cookie-jar cookie-jar)
+      (cond ((not (equalp status-code 200))
+             (error 'on-response-not-ok :text (concatenate 'string "HTTP error: " (write-to-string status-code))))
+            ((not (string= (cdr (assoc ':content-type headers)) "application/json; charset=UTF-8"))
+             (error 'on-response-not-ok :text "Not JSON content.")) 
+            (t request-result)))))
+
+(defun first-request (url)
+  (let ((drakma:*text-content-types* (cons '("application" . "json")
+                                           drakma:*text-content-types*))
+	(drakma:*drakma-default-external-format* :utf-8)
+        (cookie-jar (make-instance 'drakma:cookie-jar)))
     (multiple-value-bind (request-result status-code headers)
         (drakma:http-request url
                          :accept "application/json"
@@ -23,16 +47,17 @@
                          :method :get
                          :external-format-out :utf-8
                          :external-format-in :utf-8
-                         :redirect 100)
+                         :redirect 100
+                         :cookie-jar cookie-jar)
+      (print (drakma:cookie-jar-cookies cookie-jar))
       (cond ((not (equalp status-code 200))
              (error 'on-response-not-ok :text (concatenate 'string "HTTP error: " (write-to-string status-code))))
             ((not (string= (cdr (assoc ':content-type headers)) "application/json; charset=UTF-8"))
              (error 'on-response-not-ok :text "Not JSON content.")) 
-            (t request-result)))))
- 
+            (t (append (list request-result status-code) (drakma:cookie-jar-cookies cookie-jar)))))))
+
 (defun parse-request (json-string)
-  (let ((data (json:decode-json-from-string json-string)))
-    (get-uuid data)))
+  (json:decode-json-from-source json-string))
 
 (defun get-uuid (alist)
   (cdr (assoc :UUID (first (get-items alist)))))
