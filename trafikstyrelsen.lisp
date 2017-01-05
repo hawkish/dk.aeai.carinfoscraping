b4c1e8a92fb647735665c4e30e6a49e997038bba
Morten Høgh <mortenhogh@gmail.com>
2017-01-05 20:55:19 +0100

    Refactoring.

--- trafikstyrelsen.lisp
+++ trafikstyrelsen.lisp
@@ -15,7 +15,6 @@
     (on-response-not-ok (ex)
       (format t "An error happened: ~a~%" (text ex)))))
 
-
 (defun trafikstyrelsen-request (url)
   (let ((drakma:*text-content-types* (cons '("text" . "html")
                                            drakma:*text-content-types*))
@@ -30,46 +29,6 @@
             ((not (string= (cdr (assoc ':content-type headers)) "text/html; charset=utf-8"))
              (error 'on-response-not-ok :text "Not HTML content.")) 
             (t request-result)))))
-
-(defparameter *html*
-  "   <table id=\"tblInspections\" class=\"reports responsive\">
-                        <thead>
-                            <tr>
-                                <th>Dato</th>
-                                <th>Resultat</th>
-                                <th>Km-stand</th>
-                                <th>Reg.nr.</th>
-                                <th class=\"hideOnSmall\">Gem/send</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                
-                    <tr class=\"odd\" onclick=\"location.href=&quot;/Sider/synsrapport.aspx?Inspection=8974365&amp;Vin=11M007467&quot;\">
-						<td>12-05-2010</td>
-						<td><span class=\"noLink infoIcon\" title='Godkendt'>GOD</span></td>
-						<td>65.000</td>
-						<td>EZ12647</td>
-						<td class=\"hideOnSmall\">
-                            <a id=\"ctl00_m_g_6f9923c7_5864_4e71_bcd8_017cfab1946d_ctl00_rptInspections_ctl01_lnkSave\" class=\"saveIcon\" href=\"/_layouts/Web.Inspections/SaveReport.aspx?Inspection=8974365&amp;Vin=11M007467\"></a>
-                            <a id=\"ctl00_m_g_6f9923c7_5864_4e71_bcd8_017cfab1946d_ctl00_rptInspections_ctl01_lnkSend\" class=\"sendIcon send popup\" href=\"../_CONTROLTEMPLATES/Web.Inspections.WebParts/SearchResultWebPart/#8974365\"></a>
-                        </td>
-					</tr>
-					
-                
-                    <tr class=\"even\" onclick=\"location.href=&quot;/Sider/synsrapport.aspx?Inspection=509709&amp;Vin=11M007467&quot;\">
-						<td>10-10-2005</td>
-						<td><span class=\"noLink infoIcon\" title='Godkendt'>GOD</span></td>
-						<td></td>
-						<td></td>
-						<td class=\"hideOnSmall\">
-                            <a id=\"ctl00_m_g_6f9923c7_5864_4e71_bcd8_017cfab1946d_ctl00_rptInspections_ctl02_lnkSave\" class=\"saveIcon\" href=\"/_layouts/Web.Inspections/SaveReport.aspx?Inspection=509709&amp;Vin=11M007467\"></a>
-                            <a id=\"ctl00_m_g_6f9923c7_5864_4e71_bcd8_017cfab1946d_ctl00_rptInspections_ctl02_lnkSend\" class=\"sendIcon send popup\" href=\"../_CONTROLTEMPLATES/Web.Inspections.WebParts/SearchResultWebPart/#509709\"></a>
-                        </td>
-					</tr>
-					
-                
-                        </tbody>
-                    </table>")
 
 (defun get-surveyor-links (dom)
   (mapcar #'replace-extra-tags
